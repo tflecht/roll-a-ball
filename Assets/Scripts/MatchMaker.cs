bc0ca246054fb5bf8acd0c142c8de91b0771958d
Tim Flechtner <tflecht@gmail.com>
2016-03-26 15:55:44 -0700

    smoothed out network characer's movement by lerping its position and rotation on update

---
+++ Assets/Scripts/MatchMaker.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+using Photon;
+
+public class MatchMaker : PunBehaviour {
+
+	public Camera playerCamera;
+	public Text countText;
+	public Text winText;
+
+	public override void OnJoinedLobby()
+	{
+		Debug.Log ("Joined the lobby!");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log ("Joined a room!!!!");
+		GameObject player = PhotonNetwork.Instantiate("Player", new Vector3(0f, 0.5f, 0f), Quaternion.identity, 0);
+		PlayerController player_controller = player.GetComponent<PlayerController> ();
+		player_controller.enabled = true;
+		player_controller.countText = countText;
+		player_controller.winText = winText;
+
+		CameraController camera_controller = playerCamera.GetComponent<CameraController> ();
+		camera_controller.enabled = true;
+		camera_controller.TrackPlayer (player);
+	}
+		
+	// Private
+	// -------
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+
+	void onGUI()
+	{
+		// GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+		Debug.Log (PhotonNetwork.connectionStateDetailed.ToString());
+	}
+		
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Couldn't join a room, so making one.");
+		PhotonNetwork.CreateRoom (null);
+	}
+
+}
---
+++ Assets/Scripts/NetworkCharacter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Photon;
+
+public class NetworkCharacter : Photon.MonoBehaviour
+{
+
+	private Vector3 correctPlayerPos;
+	private Quaternion correctPlayerRot;
+
+	void Update()
+	{
+		if (!photonView.isMine)
+		{
+			transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+		}
+	}
+		
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}
+		else
+		{
+			// Network player, receive data
+			this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+			this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
